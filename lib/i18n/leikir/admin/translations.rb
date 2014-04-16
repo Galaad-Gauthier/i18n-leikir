ffd1597d14eac9197abff7845f37213230fade40
Galaad Gauthier <leikir-dev-2>
2014-04-16 16:10:50 +0200

    Added batch actions to Texts

---
+++ lib/i18n/leikir/admin/translations.rb
@@ -0,0 +1,50 @@
+ActiveAdmin.register Translation do
+  menu label: "Texts"
+
+  actions :index, :edit, :update, :show
+
+  index do
+    selectable_column
+    column :locale
+    column :key
+    column :value
+    default_actions
+  end
+
+  config.batch_actions = true
+  batch_action :destroy, false
+  batch_action :edit_translation, form: {new_translation: :textarea } do |ids, inputs|
+     Translation.find(ids).each do |ts|
+       ts.value = inputs[:new_translation]
+       ts.save!
+     end
+     redirect_to :back, notice: "update ok"
+   end
+
+  show do |t|
+    attributes_table do
+      row :locale
+      row :key
+      row :value do
+        raw t.value
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :locale
+      f.input :key
+      f.input :value, as: :html_editor
+    end
+
+    f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit(:translation => [:locale, :key, :value, :interpolations, :is_proc])
+    end
+  end
+
+end
--- lib/i18n/leikir/config/active_admin.rb
+++ lib/i18n/leikir/config/active_admin.rb
@@ -1,48 +1,48 @@
-ActiveAdmin.register Translation do
-  menu label: "Texts"
+# ActiveAdmin.register Translation do
+#   menu label: "Texts"
 
-  actions :index, :edit, :update, :show
+#   actions :index, :edit, :update, :show
 
-  index do
-    column :locale
-    column :key
-    column :value
-    default_actions
-  end
+#   index do
+#     column :locale
+#     column :key
+#     column :value
+#     default_actions
+#   end
 
-  batch_action :destroy, false
-  batch_action :edit_translation do |ids, inputs|
-    Translation.find(ids).each do |ts|
-      ts.value = inputs[:new_translation]
-      ts.save!
-    end
-    redirect_to :back, notice: "update ok"
-  end
+#   batch_action :destroy, false
+#   batch_action :edit_translation do |ids, inputs|
+#      Translation.find(ids).each do |ts|
+#        ts.value = inputs[:new_translation]
+#        ts.save!
+#      end
+#      redirect_to :back, notice: "update ok"
+#    end
 
-  show do |t|
-    attributes_table do
-      row :locale
-      row :key
-      row :value do
-        raw t.value
-      end
-    end
-  end
+#   show do |t|
+#     attributes_table do
+#       row :locale
+#       row :key
+#       row :value do
+#         raw t.value
+#       end
+#     end
+#   end
 
-  form do |f|
-    f.inputs do
-      f.input :locale
-      f.input :key
-      f.input :value, as: :html_editor
-    end
+#   form do |f|
+#     f.inputs do
+#       f.input :locale
+#       f.input :key
+#       f.input :value, as: :html_editor
+#     end
 
-    f.actions
-  end
+#     f.actions
+#   end
 
-  controller do
-    def permitted_params
-      params.permit(:translation => [:locale, :key, :value, :interpolations, :is_proc])
-    end
-  end
+#   controller do
+#     def permitted_params
+#       params.permit(:translation => [:locale, :key, :value, :interpolations, :is_proc])
+#     end
+#   end
 
-end
+# end
