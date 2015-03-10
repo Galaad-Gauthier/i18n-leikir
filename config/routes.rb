bd88bac2c6c80e45f98f57f37dddd319d4ee42da
Galaad Gauthier <galaad@leikir-dev-2>
2015-03-10 17:08:10 +0100

    Finished light version ?

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-I18n::Leikir::Engine.routes.draw do
-
-  # put "/update_value/:id" => "translations#update_value", as: "update_value"
-
-end
--- lib/i18n/leikir/version.rb
+++ lib/i18n/leikir/version.rb
@@ -1,7 +1,7 @@
 module I18n
   
   module Leikir
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 
 end
